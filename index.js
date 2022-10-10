9ef529b4faa44ddecfa328b8c3cfeb1835b02d90
pradeep <pbpradeeptechno@gmail.com>
2022-10-10 16:40:40 +0100

    ejs view engine

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
